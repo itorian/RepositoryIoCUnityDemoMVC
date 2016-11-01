27329004057174654ac71e2ceeb13305fd312e0d
itorian <itorian@live.in>
2016-11-01 10:35:07 +0530

    typo

--- RepositoryIoCUnityInMVC/RepositoryDemoMVC/App_Start/UnityConfig.cs
+++ RepositoryIoCUnityInMVC/RepositoryDemoMVC/App_Start/UnityConfig.cs
@@ -1,17 +1,14 @@
 using System;
 using Microsoft.Practices.Unity;
-using Microsoft.Practices.Unity.Configuration;
 using RepositoryDemo.Repository;
 using RepositoryDemo.Data;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity.Mvc;
 
 namespace RepositoryDemo.Web.App_Start
 {
-    /// <summary>
-    /// Specifies the Unity configuration for the main container.
-    /// </summary>
     public class UnityConfig
     {
-        #region Unity Container
         private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
         {
             var container = new UnityContainer();
@@ -19,27 +16,18 @@ namespace RepositoryDemo.Web.App_Start
             return container;
         });
 
-        /// <summary>
-        /// Gets the configured Unity container.
-        /// </summary>
         public static IUnityContainer GetConfiguredContainer()
         {
             return container.Value;
         }
-        #endregion
 
-        /// <summary>Registers the type mappings with the Unity container.</summary>
-        /// <param name="container">The unity container to configure.</param>
-        /// <remarks>There is no need to register concrete types such as controllers or API controllers (unless you want to 
-        /// change the defaults), as Unity allows resolving a concrete type even if it was not previously registered.</remarks>
         public static void RegisterTypes(IUnityContainer container)
         {
-            // NOTE: To load from web.config uncomment the line below. Make sure to add a Microsoft.Practices.Unity.Configuration to the using statements.
-            container.LoadConfiguration();
-
-            // TODO: Register your types here
+            // Register your types here
             container.RegisterType<IRepository<Customer, string>, CustomerRepository>();
 
+
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
     }
 }
